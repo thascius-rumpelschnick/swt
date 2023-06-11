6deafe5b2ed7d27debfcabcb00cbacaf16324788
Florian Zapf <zapforama@googlemail.com>
2023-06-11 23:07:10 +0200

    [08-Testen] Testing
    - Add logger
    - Add second main method for testing logger

---
+++ src/main/java/org/testing/Main.java
@@ -0,0 +1,10 @@
+package org.testing;
+
+public class Main {
+
+  public static void main(String[] args) {
+    final var publisher = new PublishingHouse();
+    publisher.calculateRoyaltiesFromTextLength("TEXT");
+  }
+
+}
--- src/main/java/org/testing/PublishingHouse.java
+++ src/main/java/org/testing/PublishingHouse.java
@@ -1,12 +1,19 @@
 package org.testing;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Objects;
 
 
 public class PublishingHouse {
 
+  private static final Logger LOGGER = LogManager.getLogger(PublishingHouse.class);
+
   public int calculateRoyaltiesFromTextLength(final String text) {
     Objects.requireNonNull(text, "No text provided.");
+
+    LOGGER.error("The text is {}", text);
 
     final var length = text.length();
 
